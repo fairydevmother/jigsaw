37708fb850ccb04441efbaa26ba316af332b5d83
fairydevmother <smkaraca06@gmail.com>
2023-02-20 06:56:25 +0300

    mobile improvement seventeenth try

--- components/postModel.jsx
+++ components/postModel.jsx
@@ -52,10 +52,10 @@ const Post = ({post}) => {
 
              </div>
              
-              <div className="container pl-8 py-10 mx-auto">
+              <div className="container mx-auto">
                <div className="xl:w-1/2 lg:w-3/4  w-full mx-auto">
               
-               <h1 className='title font-bold text-2xl'>  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
+               <h1 className='title font-bold text-2xl'>  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="svg inline-block text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                </svg>  {post.title}</h1>
               
@@ -118,18 +118,22 @@ const Post = ({post}) => {
         z-index: 1; 
         color:black;
         width:80.3rem;
-
+       
     }
     .date{
       margin-left:3.5rem;
     }
     .popup-buttonTwo{display:none;}
-    
+    .svg{
+      width:2rem;
+      height:2rem;
+    }
    
     .container{
         position: fixed;
         top: 4%;
         left:12%;
+
     }
     .title{
         font-size:1.50rem;
@@ -167,6 +171,10 @@ const Post = ({post}) => {
         color:black;
         width:55.3rem;
 
+    }
+    .svg{
+      width:2rem;
+      height:2rem;
     }
     .date{
       margin-left:3.5rem;
@@ -223,6 +231,10 @@ const Post = ({post}) => {
     .popup-button{
        display:none;
     }
+    .svg{
+      width:2rem;
+      height:2rem;
+    }
     .popup-buttonTwo{
       position: fixed;
         top:2%;
@@ -268,7 +280,7 @@ const Post = ({post}) => {
        left:2%;
    }
     .container{
-      width:55%
+      width:50%
       position: fixed;
       top:50%;
       left:12%;
@@ -276,6 +288,10 @@ const Post = ({post}) => {
     }
     .date{
       margin-left:5.5rem;
+    }
+    .svg{
+      width:1rem;
+      height:1rem;
     }
     .writer{
       margin-left:2.2rem;
